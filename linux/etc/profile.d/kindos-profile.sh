1ca28e0e67c2ab72584e4125258c47b1afb9c68d
João Pinto <lamego.pinto@gmail.com>
2023-08-06 19:12:58 +0200

    improved sdkinit info

--- linux/etc/profile.d/kindos-profile.sh
+++ linux/etc/profile.d/kindos-profile.sh
@@ -6,17 +6,21 @@ INFO='\033[1;34m'
 RESET='\e[0m' # Reset
 
 
-function print_version_info() {
+function print_info() {
   VERSION=$(cat /etc/kindos/system-profile-version)
   DATE=$(cat /etc/kindos/system-profile-date)
 
   echo -e "KindOS Workspace version ${INFO}${VERSION}${RESET}, last updated: ${INFO}${DATE}${RESET}"
+  sdkinit --installed
 }
+
+# Get list of installed SDKs
+installed_sdks=($(ls -d /usr/local/sdkinit/* | xargs -n 1 basename| grep -v "\.files"))
 
 
 if [ -r ~/.kindos.enable ]; then
   if [ -d /etc/profile.d/kindos.d ]; then
-    print_version_info
+    print_info
     for i in /etc/profile.d/kindos.d/*.sh; do
       if [ -r $i ]; then
         . $i
