3b00d4d2a9fe8d8d6d2a602d712722147469e88a
João Pinto <lamego.pinto@gmail.com>
2023-08-06 13:55:18 +0200

    install missing packages

--- debian12/provision.sh
+++ debian12/provision.sh
@@ -5,6 +5,16 @@ cd "$(dirname "$0")"
 
 LOCALE=${1:-en_US.UTF-8}
 
+REQUIRE_PACKAGES=$(grep -vE '^\s*#' requirements.txt)
+
+# Install packages
+MISSING=""
+for PACKAGE in $REQUIRE_PACKAGES; do
+    dpkg -s $PACKAGE >/dev/null 2>&1 || MISSING="$MISSING $PACKAGE"
+done
+[[ -n "$MISSING" ]] && apt-get update && apt-get install -y $MISSING
+
+
 cat <<EOF > /etc/default/locale
 LANG="en_US.UTF-8"
 LC_MESSAGES="en_US.UTF-8"
